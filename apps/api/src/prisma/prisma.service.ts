91e1b362df51c1049edb6b3ddbe1bd4b41153504
Palla Chetana Reddy <chetanareddy.palla_2027@woxsen.edu.in>
2026-04-18 19:45:13 +0530

    fix: rewrite PrismaService for Prisma 7 adapter compatibility, add postinstall

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,21 +1,36 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require('@prisma/client');
+
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private readonly client: any;
+
+  readonly retroBoard: any;
+  readonly retroSection: any;
+  readonly retroItem: any;
+
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL!,
     });
-    super({ adapter });
+    this.client = new PrismaClient({ adapter });
+    this.retroBoard = this.client.retroBoard;
+    this.retroSection = this.client.retroSection;
+    this.retroItem = this.client.retroItem;
+  }
+
+  $transaction(...args: any[]) {
+    return this.client.$transaction(...args);
   }
 
   async onModuleInit() {
-    await this.$connect();
+    // connection managed by adapter
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.client.$disconnect?.();
   }
 }
